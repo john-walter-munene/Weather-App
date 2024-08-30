32c6babc489e43d8108643b15b2483d8ae335d7d
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2024-08-30 15:52:54 +0300

    On populating UI with user request.

---
+++ resources.bundle.js
@@ -0,0 +1,66 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/resources/utility.js":
+/*!**********************************!*\
+  !*** ./src/resources/utility.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   limitToTwoWords: () => (/* binding */ limitToTwoWords)\n/* harmony export */ });\nfunction limitToTwoWords(inputString) {\r\n    const words = inputString.split(' ');\r\n    const limitedString = words.slice(0, 2).join(' ');\r\n    return limitedString;\r\n  }\r\n\r\n// console.log(limitToTwoWords('Munene, Popokabaka, Kwango, République Démocratique du Congo'))\r\n\r\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcmVzb3VyY2VzL3V0aWxpdHkuanMiLCJtYXBwaW5ncyI6Ijs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9kZXZzdGFydGJ1aWxkaW5nLy4vc3JjL3Jlc291cmNlcy91dGlsaXR5LmpzPzc0NzgiXSwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gbGltaXRUb1R3b1dvcmRzKGlucHV0U3RyaW5nKSB7XHJcbiAgICBjb25zdCB3b3JkcyA9IGlucHV0U3RyaW5nLnNwbGl0KCcgJyk7XHJcbiAgICBjb25zdCBsaW1pdGVkU3RyaW5nID0gd29yZHMuc2xpY2UoMCwgMikuam9pbignICcpO1xyXG4gICAgcmV0dXJuIGxpbWl0ZWRTdHJpbmc7XHJcbiAgfVxyXG5cclxuLy8gY29uc29sZS5sb2cobGltaXRUb1R3b1dvcmRzKCdNdW5lbmUsIFBvcG9rYWJha2EsIEt3YW5nbywgUsOpcHVibGlxdWUgRMOpbW9jcmF0aXF1ZSBkdSBDb25nbycpKVxyXG5cclxuZXhwb3J0IHsgbGltaXRUb1R3b1dvcmRzIH07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/resources/utility.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/resources/utility.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
